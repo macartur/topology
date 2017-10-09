2f528bad609c1b4cf33cf888a044afd819290c4b
Diego Rabatone Oliveira <diraol@ncc.unesp.br>
2017-10-09 12:43:01 -0300

    Add REST endpoints.
     - get_device (/api/kytos/topology/device)
     - get_link (/api/kytos/topology/link)
     - get_topology (/api/kytos/topology/)
    
    Related to #2

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 Manage the network topology
 """
+import json
 
-from kytos.core import KytosNApp, log
+from kytos.core import KytosNApp, log, rest
 
+from napps.kytos.topology.models import Device, Interface, Port, Topology
 from napps.kytos.topology import settings
 
 
@@ -22,10 +24,10 @@ class Main(KytosNApp):
 
         So, if you have any setup routine, insert it here.
         """
-        pass
+        self.topology = Topology()
 
     def execute(self):
-        """This method is executed right after the setup method execution.
+        """Execute right after the setup method execution.
 
         You can also use this method in loop mode if you add to the above setup
         method a line like the following example:
@@ -35,8 +37,47 @@ class Main(KytosNApp):
         pass
 
     def shutdown(self):
-        """This method is executed when your napp is unloaded.
+        """Execute when your napp is unloaded.
 
         If you have some cleanup procedure, insert it here.
         """
-        pass
+        log.info('NApp kytos/topology shutting down.')
+
+    @rest('devices')
+    def get_device(self):
+        """Return a json with all the devices in the topology.
+
+        Responsible for the /api/kytos/topology/devices endpoint.
+
+        e.g. [<list of devices>]
+
+        Returns:
+            string: json with all the devices in the topology
+
+        """
+        return json.dumps([device.to_json()
+                           for device in self.topology.devices])
+
+    @rest('links')
+    def get_links(self):
+        """Return a json with all the links in the topology.
+
+        Responsible for the /api/kytos/topology/links endpoint.
+
+        Returns:
+            string: json with all the links in the topology.
+
+        """
+        return json.dumps([link.to_json() for link in self.topology.links])
+
+    @rest('')
+    def get_topology(self):
+        """Return full topology.
+
+        Responsible for the /api/kytos/topology endpoint.
+
+        Returns:
+            string: json with the full topology.
+
+        """
+        return json.dumps(self.topology.to_json())
