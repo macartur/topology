d75507f9c16dc45c20e5ab4eeb6b4bba6a388394
Renan Rodrigo <renanrb@ime.usp.br>
2017-10-24 19:35:12 -0200

    Improve examples and documentation

--- settings.py
+++ settings.py
@@ -1,9 +1,18 @@
 # This is the Path to a json file to be preloaded during napp startup.
-# This file has the links aliases to be displayed for shorter outputs, if
-# needed.
-CUSTOM_CIRCUITS_PATH = '/tmp/test.json'
+# This file must have a list of circuits, with their names (or aliases), list
+# of hops and their custom properties. For a detailed example, see
+# etc/circuits.json.sample
+CUSTOM_CIRCUITS_PATH = 'etc/circuits.json'
 
-# Set default values for custom properties, for all single link circuits
+# Set default values for custom properties, for all single link circuits.
+# Example:
+#{
+#   'weigth': 20,
+#   'adm_cost':33
+#}
+#
+# This applies those values to all single-link circuits, unless those are
+# defined in the CUSTOM_CIRCUITS file.
 CUSTOM_PROPERTY_DEFAULTS = {
 }
 
