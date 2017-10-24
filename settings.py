0915dba0e923c4e93c3c2416abca7b307e02d13c
Renan Rodrigo <renanrb@ime.usp.br>
2017-10-24 09:10:51 -0200

    Add default custom property values to settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,11 @@
 # This is the Path to a json file to be preloaded during napp startup.
 # This file has the links aliases to be displayed for shorter outputs, if
 # needed.
-CUSTOM_LINKS_PATH='/tmp/test.json'
+CUSTOM_LINKS_PATH = '/tmp/test.json'
+
+# Set default values for custom properties, for all single link circuits
+CUSTOM_PROPERTY_VALES = {
+}
 
 # Set this option to true if you need the topology with bi-directional links
-DISPLAY_FULL_DUPLEX_LINKS=True
+DISPLAY_FULL_DUPLEX_LINKS = True
